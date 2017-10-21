db929b723cc8f4708021cd10685a5122b47a1b32
Alex Levitsky <mediartweb@gmail.com>
2017-10-21 13:27:02 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Device } from '@ionic-native/device';
+import { NativeStorage } from '@ionic-native/native-storage';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Device, 
+    NativeStorage,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,123 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform, AlertController } from 'ionic-angular';
+import { Device } from '@ionic-native/device';
+import { NativeStorage } from '@ionic-native/native-storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+title: string = "Device's model detection";
+myDevice : MyDevice;
 
-  constructor(public navCtrl: NavController) {
+
+  constructor(private platform: Platform, public navCtrl: NavController, public alertCtrl: AlertController, private device: Device, private nativeStorage: NativeStorage) {
+
+    this.platform.ready().then(() => {
+      
+      // this.getParts();
+       this.getFromStorage();
+      
+            
+      });
 
   }
 
+  getFromStorage() 
+  {
+    this.nativeStorage.getItem('myDevice')
+      .then(
+        data => {  
+          console.log(data);
+          this.myDevice = JSON.parse(data);
+                 },
+        error => { 
+          console.error(error);
+                }
+    );
+
+  }
+
+
+
+
+
+  ShowModel() 
+  {
+
+    this.detectModel();
+
+
+
+if(this.myDevice!==null)
+{
+
+  this.saveToStorage();
+ alert(this.myDevice.manufacturer+' '+this.myDevice.model);
+/*    const myAlert = this.alertCtrl.create({
+           title: 'Infornation',
+           message: this.myDevice.manufacturer+' '+this.myDevice.model,
+           buttons: [
+             
+             {
+               text: 'Ok',
+               handler: () => {
+                 this.saveToStorage();
+
+               }
+             }
+           ]
+         });
+         myAlert.present();
+         */
+        }
+  }
+
+  saveToStorage()
+  {
+    this.nativeStorage.setItem('myDevice', JSON.stringify(this.myDevice))
+    .then(
+      () => console.log('Stored model!'),
+      error => console.error('Error storing model to native storage', error)
+    );
+  }
+  
+
+  detectModel()
+  {
+    this.myDevice= 
+    { model : this.device.model,
+      manufacturer : this.device.manufacturer
+    }
+  }
+
+ 
+  exit()
+  {
+
+    const alert = this.alertCtrl.create({
+      title: 'Thank you',
+      message: 'Thank you for downloading our application.',
+      buttons: [
+       
+        {
+          text: 'Exit application',
+          handler: () => {
+            this.platform.exitApp();
+          }
+        }
+      ]
+    });
+    alert.present();
+
+  }
+
+  
+}
+
+export interface MyDevice
+{
+model: string;
+manufacturer : string;  
 }
